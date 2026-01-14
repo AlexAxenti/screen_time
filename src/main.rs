f05fc12a15f9f35893e3747784250f2e3b71d839
AlexAxenti <alex.axenti@gmail.com>
2026-01-14 01:27:50 -0500

    Get exe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{thread::sleep, time::Duration};
 
-use windows::Win32::UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowTextLengthW, GetWindowTextW};
+use windows::{Win32::{Foundation::CloseHandle, System::Threading::{OpenProcess, PROCESS_NAME_WIN32, PROCESS_QUERY_LIMITED_INFORMATION, QueryFullProcessImageNameW}, UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowTextLengthW, GetWindowTextW, GetWindowThreadProcessId}}, core::PWSTR};
 
 fn main() {
     loop {
@@ -25,5 +25,46 @@ fn main() {
         let window_text = String::from_utf16_lossy(&buffer);
 
         println!("Window text: {window_text}");
+
+        let mut hwnd_process_id: u32 = 0;
+        unsafe {
+            GetWindowThreadProcessId(foreground_window_hwnd, Some(&mut hwnd_process_id));
+        }
+
+        println!("Window pid: {hwnd_process_id}");
+
+        let process_handle = match unsafe {
+            OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, hwnd_process_id)
+        } {
+            Ok(handle) => {
+                handle
+            }
+            Err(e) => {
+                eprintln!("Failed to open process {e}");
+                continue;
+            }
+        };
+
+        let mut process_image_buffer = vec![0u16; 256];
+
+        let pwstr = PWSTR(process_image_buffer.as_mut_ptr());
+
+        let mut lpdwsize: u32 = 256;
+
+        if let Err(e) = unsafe {
+            QueryFullProcessImageNameW(process_handle, PROCESS_NAME_WIN32, pwstr, &mut lpdwsize)
+        } { 
+            println!("Error {e}");
+        }
+
+        let process_exe = String::from_utf16_lossy(&process_image_buffer[0..lpdwsize as usize]);
+
+        println!("Process exe: {process_exe}");
+
+        if let Err(e) = unsafe {
+            CloseHandle(process_handle)
+        } {
+            eprintln!("Failed to close handle {e}");
+        }
     }
 }
