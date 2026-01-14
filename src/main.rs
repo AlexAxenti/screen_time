5665c3d07c411be55e4da0792f887968fe350b74
AlexAxenti <alex.axenti@gmail.com>
2026-01-13 23:55:03 -0500

    constant loop

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-use windows::Win32::UI::WindowsAndMessaging::{GetForegroundWindow, GetWindow, GetWindowTextLengthW, GetWindowTextW};
+use std::{thread::sleep, time::Duration};
+
+use windows::Win32::UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowTextLengthW, GetWindowTextW};
 
 fn main() {
-    println!("Hello, world!");
+    loop {
+        sleep(Duration::from_millis(500));
 
-    let foreground_window_hwnd = unsafe {
-        GetForegroundWindow()
-    };
+        let foreground_window_hwnd = unsafe {
+            GetForegroundWindow()
+        };
 
-    let window_text_length = unsafe {
-        GetWindowTextLengthW(foreground_window_hwnd)
-    };
+        let window_text_length = unsafe {
+            GetWindowTextLengthW(foreground_window_hwnd)
+        };
 
-    let mut buffer = vec![0u16; (window_text_length + 1) as usize];
+        let mut buffer = vec![0u16; (window_text_length + 1) as usize];
 
-    println!("Window length: {window_text_length}");
+        println!("Window length: {window_text_length}");
 
-    unsafe {
-        GetWindowTextW(foreground_window_hwnd, &mut buffer)
-    };
+        unsafe {
+            GetWindowTextW(foreground_window_hwnd, &mut buffer)
+        };
 
-    let window_text = String::from_utf16_lossy(&buffer);
+        let window_text = String::from_utf16_lossy(&buffer);
 
-    println!("Window text: {window_text}");
+        println!("Window text: {window_text}");
+    }
 }
