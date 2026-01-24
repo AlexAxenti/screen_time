9853c3617f8d8790bc5b8b97b54957acef581da3
AlexAxenti <alex.axenti@gmail.com>
2026-01-24 00:55:18 -0500

    celaning code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,26 +20,11 @@ function Index() {
     <div>    
       <Typography variant="h1" sx={{textAlign: 'center'}}>Screen Time</Typography>
       <DashboardSummary epochStartOfDayMs={epochStartOfDayMs} />
-      {/* <div>
-        {windowSegments && windowSegments.map((segment, index) => (
-          <Card key={index} sx={{ 
-            display: 'flex', 
-            flexDirection: 'row', 
-            alignItems: 'center', 
-            justifyContent: 'space-between',
-            padding: 2,
-            marginBottom: 2
-          }}>
-            <Typography>{segment.window_exe}</Typography>
-            <Typography>Duration: {Math.floor(segment.duration / 60000)} minutes</Typography>
-          </Card>
-        ))}
-      </div> */}
 
       <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>  
         <TopExesChart epochStartOfDayMs={epochStartOfDayMs} />
         <UsageFragmentationChart epochStartOfDayMs={epochStartOfDayMs} />
       </Box>
     </div>
-  )
-}
+  );
+};
