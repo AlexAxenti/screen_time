fa254cba2ce289aef47ebd4b784765950f3c4383
AlexAxenti <alex.axenti@gmail.com>
2026-02-07 23:00:04 -0500

    Temp removed advanced search nav

--- src/components/AppHeader/components/NavItems.ts
+++ src/components/AppHeader/components/NavItems.ts
@@ -6,7 +6,7 @@ export interface NavItem {
 export const NAV_ITEMS: NavItem[] = [
 	{ label: "Dashboard", to: "/" },
 	{ label: "Applications", to: "/applications" },
-	{ label: "Advanced Search", to: "/advanced-search" },
+	// { label: "Advanced Search", to: "/advanced-search" },
 	// { label: "Trends", to: "/trends" },
 	{ label: "Settings", to: "/settings" },
 ];
