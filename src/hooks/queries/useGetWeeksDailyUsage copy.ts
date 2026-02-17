f89100f3f8c245050b4d2d76cd9779a6f00832a4
AlexAxenti <alex.axenti@gmail.com>
2026-02-17 18:12:16 -0500

    New chart

---
+++ src/hooks/queries/useGetWeeksDailyUsage copy.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { invoke } from "@tauri-apps/api/core";
+import type { AvgTimeOfDayUsage } from "../../types/dto";
+
+const useGetAvgTimeOfDayUsage = (startTime: number, endTime: number, appId: string) => {
+	return useQuery({
+		queryKey: ["avg_time_of_day_usage", startTime, endTime, appId],
+		queryFn: async () => {
+			try {
+				let avgTimeOfDayUsage: AvgTimeOfDayUsage[] = await invoke(
+					"get_app_avg_time_of_day_usage",
+					{ startTime, endTime, appId },
+				);
+				return avgTimeOfDayUsage;
+			} catch (e) {
+				console.error("get app_avg_time_of_day_usage invoke failed");
+				throw e;
+			}
+		},
+	});
+};
+
+export default useGetAvgTimeOfDayUsage;
---
+++ src/routes/applications/$exe/-components/AvgTimeOfDayUsageChart.tsx
@@ -0,0 +1,42 @@
+import { Box } from "@mui/material";
+import useGetAvgTimeOfDayUsage from "../../../../hooks/queries/useGetWeeksDailyUsage copy";
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
+interface UsageFragmentationChartProps {
+	startOfRangeMs: number;
+	endOfRangeMs: number;
+	appId: string;
+}
+
+const AvgTimeOfDayUsageChart = ({
+	startOfRangeMs,
+	endOfRangeMs,
+	appId,
+}: UsageFragmentationChartProps) => {
+  const { data: avgUsage } = useGetAvgTimeOfDayUsage(startOfRangeMs, endOfRangeMs, appId);
+
+  return (
+    <Box sx={{ width: "100%", height: "300px" }}>
+			<ResponsiveContainer width="100%" height="100%">
+				<LineChart data={avgUsage}>
+          <CartesianGrid vertical={false} />
+
+          <XAxis dataKey="hour" />
+
+          <YAxis />
+
+          <Tooltip />
+
+          <Line
+            type="monotone"
+            dataKey="avg_ms_per_hour_of_day"
+            dot={false}
+            strokeWidth={2}
+          />
+        </LineChart>
+			</ResponsiveContainer>
+		</Box>
+  );
+};
+
+export default AvgTimeOfDayUsageChart;
